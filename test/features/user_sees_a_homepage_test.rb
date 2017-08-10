8b1a2054fe5163bd316106c14216b95fcad749b9
Mike Heft <mikeheft@gmail.com>
2017-08-10 14:46:55 -0600

    work on about

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -16,6 +16,9 @@ class HomepageTest < CapybaraTestCase
   end
 
   def test_user_can_see_the_about_page
-    visit 
+    visit '/about'
+
+    assert page.has_content?("About Me!")
+    assert_equal 200, page.status_code
   end
 end
